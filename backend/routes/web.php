a16bab68c9a86836a263c457cdcb3c5d93b49e6c
Osama Ali <osama.all.shawi@gmail.com>
2026-05-20 11:24:31 +0300

    update on wep.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+Route::get('/', function () {
+    return response()->json([
+        'status' => 'Laravel API running successfully'
+    ]);
+});
 Route::get('/', function () {
     return view('welcome');
 });
