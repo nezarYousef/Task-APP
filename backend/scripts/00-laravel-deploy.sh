0ba95010468065d397a0476b190ba5fc6a7463ea
Osama Ali <osama.all.shawi@gmail.com>
2026-05-20 12:30:57 +0300

    I updata 00-laravel-deploy

--- backend/scripts/00-laravel-deploy.sh
+++ backend/scripts/00-laravel-deploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+
+echo "Installing dependencies..."
+composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
+
+echo "Generating APP KEY if missing..."
+php artisan key:generate --force
+
+echo "Clearing old caches..."
+php artisan config:clear
+php artisan route:clear
+php artisan cache:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -10,3 +19,5 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Deployment done 🚀"
